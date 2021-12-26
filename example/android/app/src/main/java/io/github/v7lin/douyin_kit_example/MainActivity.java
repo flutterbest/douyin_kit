7ab208f4e06b375fc4283583ce4eee3c4c369281
匿名 <>
2021-12-26 17:26:22 +0800

    增加授权回调 升级空安全

--- example/android/app/src/main/java/io/github/v7lin/douyin_kit_example/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package io.github.v7lin.douyin_kit_example;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,5 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:douyin_kit/douyin_kit.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,10 +11,18 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
+  String res = "";
   @override
   void initState() {
     super.initState();
+    Douyin.instance.registerApp(clientKey: 'aw6sdvh96ng2gdsk');
+    Douyin.instance.respStream().listen(_listenLogin);
+  }
+
+  void _listenLogin(dynamic data) {
+    setState(() {
+      res = data.toString();
+    });
   }
 
   @override
@@ -26,6 +31,23 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Douyin Kit Demo'),
+        ),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Douyin.instance.auth(scope: ['user_info']);
+                },
+                child: const Text('授权'),
+              ),
+              Text(
+                res,
+                style: const TextStyle(color: Colors.black),
+              ),
+            ],
+          ),
         ),
       ),
     );
